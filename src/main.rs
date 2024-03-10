e094170a4ae77ec42e3167c68ecc6108c7133b5a
Wade S Duvall <ackthet@gmail.com>
2024-03-10 10:58:12 -0400

    feat(main): move from expect to env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 use booster_rustum::parser;
 use booster_rustum::Args;
+
 use clap::Parser;
 use color_eyre::eyre::Result;
+use log::error;
+
 use std::fs;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+    env_logger::init();
     Args::parse();
 
-    let file =
-        fs::File::open("config/booster-rustum.yaml").expect("Couldn't open booster-rustum.yaml");
-    let yaml: parser::Config = serde_yaml::from_reader(file).expect("Cannot serialize");
+    let file = match fs::File::open("config/boster-rustum.yaml") {
+        Ok(file) => file,
+        Err(e) => {
+            error!("Couldn't open booster-rustum.yaml: {e}");
+            return Ok(());
+        }
+    };
+    let yaml: parser::Config = match serde_yaml::from_reader(file) {
+        Ok(yaml) => yaml,
+        Err(e) => {
+            error!("Cannot serialize: {e}");
+            return Ok(());
+        }
+    };
     println!("Deserialize =\n{yaml:?}");
 
     Ok(())
