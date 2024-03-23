7923d5d2a498dd09f3fab0c4c9ff3e252ba58acb
Wade S Duvall <ackthet@gmail.com>
2024-03-23 14:32:00 -0400

    feat: use std::process::ExitCode

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,33 @@ use booster_rustum::parser;
 use booster_rustum::Args;
 
 use clap::Parser;
-use color_eyre::eyre::Result;
 use log::error;
 
 use std::fs;
+use std::process::ExitCode;
 
-fn main() -> Result<()> {
-    color_eyre::install()?;
+fn main() -> ExitCode {
+    if color_eyre::install().is_err() {
+        error!("Failed to init colo_eyre.");
+        return ExitCode::from(2);
+    };
     env_logger::init();
     Args::parse();
 
-    let file = match fs::File::open("config/boster-rustum.yaml") {
+    let file = match fs::File::open("config/booster-rustum.yaml") {
         Ok(file) => file,
         Err(e) => {
             error!("Couldn't open booster-rustum.yaml: {e}");
-            return Ok(());
+            return ExitCode::from(2);
         }
     };
     let yaml: parser::Config = match serde_yaml::from_reader(file) {
         Ok(yaml) => yaml,
         Err(e) => {
             error!("Cannot serialize: {e}");
-            return Ok(());
+            return ExitCode::from(2);
         }
     };
     println!("Deserialize =\n{yaml:?}");
-
-    Ok(())
+    ExitCode::from(0)
 }
